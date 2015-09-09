4f1dbf7d52ac755632b41b3e29b3fd64470e4b66
Maksim <dollydasheep@gmail.com>
2015-09-09 12:00:32 -0400

    Add http methods structure to Angular factory

--- app/factories/carsFactory.js
+++ app/factories/carsFactory.js
@@ -2,12 +2,24 @@
 
   var carsFactory = function($http, appSettings) {
     var factory = {};
-    factory.cars = {};
+    factory.cars = [];
+    factory.car = {};
 
+    factory.register = function(){};
+    factory.login = function(){};
 
+    factory.getCars = function(){};
+    factory.createCar = function(){};
+    factory.updateCar = function(){};
+    factory.deleteCar = function(){};
 
+    factory.createEvent = function(){};
+    factory.updateEvent = function(){};
+    factory.deleteEvent = function(){};
 
-
+    factory.createExpense = function(){};
+    factory.updateExpense = function(){};
+    factory.deleteExpense = function(){};
 
     return factory;
   };
