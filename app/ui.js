06db84314aac790cdcefd54172e8d715c9eaf9af
Maksim <dollydasheep@gmail.com>
2015-09-11 10:02:30 -0400

    Add CSS styling and events
    Modals now hide on Submit buttons click. Added Blessed custom font for project name. Logout button now works.

--- app/ui.js
+++ app/ui.js
@@ -1,3 +1,27 @@
 $('#login-close').on('click', function(){
   $('#login-password').val();
 });
+
+$('#login-button').on('click', function(){
+  $('#loginModal').modal('hide');
+});
+
+$('#register-button').on('click', function(){
+  $('#registerModal').modal('hide');
+});
+
+$('#newcar-button').on('click', function(){
+  $('#carModal').modal('hide');
+});
+
+$('#newevent-button').on('click', function(){
+  $('#new-event-modal').modal('hide');
+});
+
+$('#newevent-button').on('click', function(){
+  $('#newexpense-modal').modal('hide');
+});
+
+$('#logout-button').on('click', function(){
+  location.reload();
+});
