1b2a1b3b07f83ac40ed778d6826d0c5d3eadc73d
Maksim <dollydasheep@gmail.com>
2015-09-11 12:18:28 -0400

    Delete useless code

--- app/controllers/carsController.js
+++ app/controllers/carsController.js
@@ -9,8 +9,8 @@
     var vm = this;
     vm.appSettings = appSettings;
     vm.userCredentials = {};
-    vm.eventData = {};
-    vm.expenseData = {};
+    vm.eventData = []; // CONNECT TO FACTORY, MAKE SPECIFIC REQUEST
+    vm.expenseData = []; // CONNECT TO FACTORY, MAKE SPECIFIC REQUEST
     vm.carData = {};
     vm.cars = carsFactory.cars;
     vm.events = carsFactory.events;
@@ -21,37 +21,6 @@
     vm.carSelected = null;
     vm.chartData = carsFactory.chartData;
     vm.user = carsFactory.user;
-
-    // vm.chartData = {
-    //   labels: ["January", "February", "March", "April", "May", "June", "July"],
-    //   series: ['Foo', 'Bar'],
-    //   data: [
-    //     [65, 59, 80, 81, 56, 55, 40],
-    //     [28, 48, 40, 19, 86, 27, 90]
-    //   ]
-    // };
-    // vm.chartData.doughnutLabels = ["Download Sales", "In-Store Sales", "Mail-Order Sales"];
-    // vm.chartData.doughnutData = [300, 500, 100];
-
-    vm.searchElement = function(elements, elementId){
-      var elementFound;
-      if (elements == "events") {
-        vm.events.forEach(function(event){
-          if (event.id === +elementId) {
-            elementFound = event;
-          }
-        });
-        vm.eventData = elementFound;
-      } else if (elements == "expenses") {
-        vm.expenses.forEach(function(expense){
-          console.log('expense id is', expense.id, +elementId);
-          if (expense.id === +elementId) {
-            elementFound = expense;
-          }
-        });
-        vm.expenseData = elementFound;
-      }
-    };
 
     vm.register = function(){
       carsFactory.register(vm.userCredentials, vm.carSelected);
@@ -86,7 +55,6 @@
       carsFactory.createEvent(vm.newEvent, vm.carSelected);
     };
     vm.updateEvent = function(event){
-      vm.searchElement("events", event.target.id);
       carsFactory.updateEvent(vm.eventData, vm.carSelected);
     };
     vm.deleteEvent = function(){
@@ -99,10 +67,7 @@
       carsFactory.createExpense(vm.newExpense, vm.carSelected);
     };
     vm.updateExpense = function(event){
-      vm.searchElement("expenses", event.target.id);
-      console.log('we clicked on expense with id = ', event.target.id);
-      console.log('expenseData is now', vm.expenseData);
-      carsFactory.updateExpense(vm.expenseData, vm.carSelected);
+      carsFactory.updateExpense(vm.expenseData[0], vm.carSelected);
     };
     vm.deleteExpense = function(){
       carsFactory.deleteExpense(expenseData, vm.carSelected);
