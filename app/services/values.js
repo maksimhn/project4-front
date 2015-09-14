0e215fe4fb841110150940332792e07ff150a07a
Maksim <dollydasheep@gmail.com>
2015-09-14 16:55:29 -0400

    Bower components are visible to GitHub

--- app/services/values.js
+++ app/services/values.js
@@ -1,7 +1,10 @@
 (function valuesIIFE(){
 
   angular.module('carsApp').value('appSettings', {
-    apiURL: 'http://localhost:3000',
+    // apiURL: 'http://localhost:3000',
+    apiURL: 'https://calm-cove-8790.herokuapp.com',
+
+
     version: '1.0',
     user: []
   });
